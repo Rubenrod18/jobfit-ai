c3753608d52a6f9dacfb8d13ee1470acfe08145e
Rubén Rodríguez Ramírez <rubenrod.018@gmail.com>
2025-03-31 15:59:06 +0200

    chore: remove useless code

--- src/database.py
+++ src/database.py
@@ -13,12 +13,8 @@ from config import get_settings
 logger = logging.getLogger(__name__)
 settings = get_settings()
 
-# async_engine = create_async_engine(settings.DATABASE_URL, echo=True)
-# AsyncSessionLocal = sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
-
 engine = create_engine(settings.SYNC_DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
-
 
 client = AsyncIOMotorClient(settings.MONGODB_URI)
 database = client.get_database(settings.MONGODB_NAME)
@@ -26,15 +22,6 @@ database = client.get_database(settings.MONGODB_NAME)
 
 def mongo_init_beanie():
     init_beanie(database, document_models=[JobAnalysis, ResumeAnalysis])
-
-
-def get_db() -> SessionLocal:
-    with SessionLocal() as session:
-        try:
-            yield session
-            session.commit()
-        finally:
-            session.close()
 
 
 class SQLDatabase:
