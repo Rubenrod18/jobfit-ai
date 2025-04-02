b4eeb6b10b0964786aea9d51d44a08b38db65b34
Rubén Rodríguez Ramírez <rubenrod.018@gmail.com>
2025-04-02 12:45:42 +0200

    test: update setup_database fixture to create sqlalchemy session correctly

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -2,9 +2,9 @@ import os
 import uuid
 
 import pytest
+import sqlalchemy as sa
 from dotenv import find_dotenv, load_dotenv
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine, text
 
 from app import create_app
 from app.cli import CreateDatabaseCli
@@ -19,45 +19,51 @@ def pytest_configure():
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_database():
-    db_uri = f'{os.environ["SQLALCHEMY_DATABASE_URI"]}_{uuid.uuid4().hex}'
-    engine = create_engine(db_uri)
-    common_session.configure(bind=engine)
-    os.environ['SQLALCHEMY_DATABASE_URI'] = db_uri
+    def create_new_sqlalchemy_session() -> tuple[str, sa.Engine]:
+        test_db_uri = f'{os.environ["SQLALCHEMY_DATABASE_URI"]}_{uuid.uuid4().hex}'
+        test_engine = sa.create_engine(test_db_uri)
+        common_session.remove()
+        common_session.configure(bind=test_engine)
+        original_db_uri = os.environ['SQLALCHEMY_DATABASE_URI']
+        os.environ['SQLALCHEMY_DATABASE_URI'] = test_db_uri
+        return original_db_uri, test_engine
 
-    def create_db():
-        seeder_cli = CreateDatabaseCli(db_uri=db_uri)
+    def create_db(engine: sa.Engine):
+        seeder_cli = CreateDatabaseCli(db_uri=os.environ['SQLALCHEMY_DATABASE_URI'])
         seeder_cli.run_command()
 
         with engine.begin() as conn:
             BaseMixin.metadata.create_all(conn)
 
-    def drop_db():
-        db_name_to_drop = db_uri.rsplit('/', 1)[1]
+    def drop_db(engine: sa.Engine):
+        db_name_to_drop = os.environ['SQLALCHEMY_DATABASE_URI'].rsplit('/', 1)[1]
         neutral_engine_url = engine.url.set(database='postgres')
-        neutral_engine = create_engine(neutral_engine_url, echo=True)
+        neutral_engine = sa.create_engine(neutral_engine_url, echo=True)
 
         try:
             with neutral_engine.connect() as conn:
                 conn.execution_options(isolation_level='AUTOCOMMIT')
                 conn.execute(
-                    text(f"""
+                    sa.text(f"""
                     SELECT pg_terminate_backend(pid)
                     FROM pg_stat_activity
                     WHERE datname = '{db_name_to_drop}';
                 """)
                 )
-                conn.execute(text(f'DROP DATABASE IF EXISTS "{db_name_to_drop}"'))
+                conn.execute(sa.text(f'DROP DATABASE IF EXISTS "{db_name_to_drop}"'))
                 print(f"Database '{db_name_to_drop}' dropped successfully.")  # noqa
         finally:
             neutral_engine.dispose()
 
-    create_db()
-    yield db_uri
-    drop_db()
+    db_uri, sa_engine = create_new_sqlalchemy_session()
+    create_db(sa_engine)
+    yield
+    drop_db(sa_engine)
+    os.environ['SQLALCHEMY_DATABASE_URI'] = db_uri
 
 
 @pytest.fixture
 def client(setup_database):
     app = create_app()
-    app.container.config.from_dict({'SQLALCHEMY_DATABASE_URI': os.getenv('SQLALCHEMY_DATABASE_URI')})
+    app.container.config.from_dict({'SQLALCHEMY_DATABASE_URI': os.environ['SQLALCHEMY_DATABASE_URI']})
     yield TestClient(app)
